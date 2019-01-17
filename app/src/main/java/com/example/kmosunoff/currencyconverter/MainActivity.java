40be1954bb6014acc987f592b851ae7ea94aa8f7
kmosunoff <kmosunoff@mail.ru>
2019-01-17 13:38:33 +0300

    UI and data loading added

--- app/src/main/java/com/example/kmosunoff/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/kmosunoff/currencyconverter/MainActivity.java
@@ -1,13 +1,104 @@
 package com.example.kmosunoff.currencyconverter;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] currencies = { "RUB", "USD", "EUR" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fillSpinners();
     }
+
+    private void fillSpinners() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, currencies);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinnerFrom = findViewById(R.id.spinnerFrom);
+        Spinner spinnerTo = findViewById(R.id.spinnerTo);
+        spinnerFrom.setAdapter(adapter);
+        spinnerTo.setAdapter(adapter);
+    }
+
+    public void convert(View view) {
+        ConvertAsyncTask convertAsyncTask = new ConvertAsyncTask();
+        convertAsyncTask.execute();
+    }
+
+    public class ConvertAsyncTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            Log.i("INFO", response);
+            int value;
+            try {
+                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                value = object.getInt("USD_RUB");
+            } catch (JSONException e) {
+                value = 0;
+            }
+            TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+            resultTextView.setText(Integer.toString(value));
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                URL url = new URL("https://free.currencyconverterapi.com/api/v6/convert?q=USD_RUB&compact=ultra");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+    }
+
 }
